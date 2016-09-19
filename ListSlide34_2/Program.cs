19aa5472d2a5991c36a3abbdb03a7dbffd6fae8a
Kurt Weber <weberkrt@msn.com>
2016-09-19 19:46:07 -0400

    Homework Slide 34 #2

---
+++ ListSlide34_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSlide34_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create list of true-false
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+
+            // Go through each to decide true-false
+            foreach (bool element in boolList)
+            {
+                if (element == true)
+                {
+                    // Print out for true
+                    Console.WriteLine("Better bring an umbrella");
+                }
+                else
+                {
+                    // Print out for false
+                    Console.WriteLine("No rain today, enjoy the sunshine!");
+                }
+            }
+        }
+    }
+}
